d2484a8e5bf9def532c78c85c70b775fbdfa4bc3
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-06-22 22:01:18 +0400

    remove css link from the helmet as it does not work

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { GlobalStyle } from './config/Global.styled.ts'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { GlobalStyle } from "./config/Global.styled.ts";
+import { Helmet } from "react-helmet";
+import './leaflet.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <Helmet>
+      <script
+        src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+      ></script>
+    </Helmet>
     <GlobalStyle />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
