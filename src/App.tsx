23933528a471d2068a6fbfed459b36561b666507
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-06-23 00:17:36 +0400

    add state variables and components to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import MapComponent from "./components/MapComponent/MapComponent";
 import styled from "styled-components";
+import SelectMenu from "./components/SelectMenu/SelectMenu";
+import { RegionType, CITIES } from "./utils/cities";
+import { useState } from "react";
 
 function App() {
+  const [region, setRegion] = useState<RegionType>("european");
+  const [city, setCity] = useState<string>(CITIES[region][0].city);
+
+  const SelectMenuProps = {
+    region,
+    setRegion,
+    city,
+    setCity,
+  };
   return (
     <SContainer>
+      <SelectMenu {...SelectMenuProps} />
       <MapComponent />
     </SContainer>
   );
@@ -14,4 +27,7 @@ export default App;
 const SContainer = styled.div`
   width: 100vw;
   height: 100vh;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
