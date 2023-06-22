334016b342592baabbf26aa50cad0bfb85678ff7
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-06-23 00:27:17 +0400

    reveive props at MapComponent; display updated map on every change

--- src/components/MapComponent/MapComponent.tsx
+++ src/components/MapComponent/MapComponent.tsx
@@ -1,14 +1,15 @@
 import { LatLngExpression } from "leaflet";
-import { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import styled from "styled-components";
+import { City } from "../../utils/cities";
 
-export default function MapComponent() {
-  const [center, setCenter] = useState<LatLngExpression | undefined>([42.3154, 43.3569]);
+export default function MapComponent(props: { city: City }) {
+  const city = props.city;
+  const center: LatLngExpression | undefined = [city.lat, city.lng];
 
   return (
     <SDiv>
-      <MapContainer center={center} zoom={7} scrollWheelZoom={true} id="map">
+      <MapContainer key={center.toString()} center={center} zoom={10} scrollWheelZoom={true} id="map">
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -18,12 +19,15 @@ export default function MapComponent() {
   );
 }
 
-
 const SDiv = styled.div`
- width: 100vw;
- height: 100vh;
-  &>div{ // leaflet container
-    width: 100vw;
-    height: 100vh;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  & > div {
+    // leaflet container
+    width: 80%;
+    height: 80%;
   }
-`
+`;
