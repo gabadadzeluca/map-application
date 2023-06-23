83f13e83c96d43f978c4b9bb36335a2a88580b81
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-06-23 20:30:59 +0400

    add airports array of tuples

---
+++ src/utils/airports.ts
@@ -0,0 +1,24 @@
+import { LatLngTuple } from "leaflet";
+
+export const AIRPORTS:LatLngTuple[]  = [
+  [40.6413, -73.7781],          // John F. Kennedy International Airport - New York, USA
+  [51.4700, -0.4543],           // Heathrow Airport - London, UK
+  [25.2532, 55.3657],           // Dubai International Airport - Dubai, UAE
+  [35.5494, 139.7798],          // Tokyo Haneda Airport - Tokyo, Japan
+  [40.0799, 116.6031],          // Beijing Capital International Airport - Beijing, China
+  [33.9416, -118.4085],         // Los Angeles International Airport - Los Angeles, USA
+  [49.0097, 2.5479],            // Charles de Gaulle Airport - Paris, France
+  [-33.9461, 151.1772],         // Sydney Airport - Sydney, Australia
+  [50.0379, 8.5622],            // Frankfurt Airport - Frankfurt, Germany
+  [1.3644, 103.9915],           // Singapore Changi Airport - Singapore
+  [52.3105, 4.7683],            // Amsterdam Airport Schiphol - Amsterdam, Netherlands
+  [22.3080, 113.9185],          // Hong Kong International Airport - Hong Kong
+  [43.6777, -79.6248],          // Toronto Pearson International Airport - Toronto, Canada
+  [13.6813, 100.7475],          // Suvarnabhumi Airport - Bangkok, Thailand
+  [37.4602, 126.4407],          // Incheon International Airport - Seoul, South Korea
+  [48.3537, 11.7750],           // Munich Airport - Munich, Germany
+  [40.4918, -3.5690],           // Madrid Barajas Airport - Madrid, Spain
+  [41.9742, -87.9073],          // O'Hare International Airport - Chicago, USA
+  [41.8003, 12.2389],           // Leonardo da Vinci-Fiumicino Airport - Rome, Italy
+  [41.2756, 28.7519]            // Istanbul Airport - Istanbul, Turkey
+];
