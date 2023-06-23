ade4629bb7c329c77aba3fe1c6dc05ac3d4448ae
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-06-23 20:22:17 +0400

    add a button to toggle airports

--- src/components/ModeChangeComponent.tsx
+++ src/components/ModeChangeComponent.tsx
@@ -3,12 +3,17 @@ import styled from 'styled-components'
 export default function ModeChangeComponent(props:{
   markerOn: boolean;
   setMarkerOn:React.Dispatch<React.SetStateAction<boolean>>;
+  displayAirports: boolean;
+  setDisplayAirports: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
-  const {markerOn, setMarkerOn} = props;
+  const {markerOn, setMarkerOn, displayAirports, setDisplayAirports} = props;
   return (
     <SDiv>
       <SButton onClick={()=>setMarkerOn(!markerOn)}>
         Marker mode: {markerOn ? 'ON' : 'OFF'}
+      </SButton>
+      <SButton onClick={()=>setDisplayAirports(!displayAirports)}>
+        Show Airports: {displayAirports ? 'ON' : 'OFF'}
       </SButton>
     </SDiv>
   )
@@ -19,5 +24,9 @@ const SDiv = styled.div`
 `
 
 const SButton = styled.button`
-  
+  padding: .5rem 1rem;
+  border-radius: .5rem;
+  border: none;
+  background-color: blue;
+  color: white;
 `
