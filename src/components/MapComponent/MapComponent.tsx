42e092b2d8791e559d8ed186923f302d1f7ee6af
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-06-23 15:14:50 +0400

    add click handler event; add and remove markers by left/right clicks

--- src/components/MapComponent/MapComponent.tsx
+++ src/components/MapComponent/MapComponent.tsx
@@ -1,19 +1,75 @@
-import { LatLngExpression } from "leaflet";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { LatLngExpression, LatLngTuple, LeafletMouseEvent } from "leaflet";
+import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
 import styled from "styled-components";
 import { City } from "../../utils/cities";
+import { useReducer, useRef, useEffect } from "react";
+import { markerReducer, ACTIONS } from "../../utils/MarkerReducer";
 
 export default function MapComponent(props: { city: City }) {
   const city = props.city;
-  const center: LatLngExpression | undefined = [city.lat, city.lng];
+  const center: LatLngExpression = [city.lat, city.lng];
+  const [markers, dispatch] = useReducer(markerReducer, []);
+  const mapRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // remove right click context menu event
+    const handleContextMenu = (event: MouseEvent) => {
+      event.preventDefault();
+    };
+    // Add the event listener when the component mounts
+    mapRef.current &&
+      mapRef.current.addEventListener("contextmenu", handleContextMenu);
+
+    // Remove the event listener when the component unmounts
+    return () => {
+      mapRef.current &&
+        mapRef.current.removeEventListener("contextmenu", handleContextMenu);
+    };
+  }, []);
+
+  const handleMapClick = (e: LeafletMouseEvent): void => {
+    const { lat, lng } = e.latlng;
+    const latlngTuple: LatLngTuple = [
+      parseFloat(lat.toFixed(2)),
+      parseFloat(lng.toFixed(2)),
+    ];
+    if (e.originalEvent.button === 0) {
+      dispatch({
+        type: ACTIONS.ADD_MARKER,
+        payload: latlngTuple,
+      });
+    } else if (e.originalEvent.button === 2) {
+      dispatch({
+        type: ACTIONS.REMOVE_MARKER,
+      });
+    }
+  };
+
+  const MapClickHandler = () => {
+    useMapEvents({
+      mousedown: handleMapClick,
+    });
+
+    return null;
+  };
 
   return (
-    <SDiv>
-      <MapContainer key={center.toString()} center={center} zoom={10} scrollWheelZoom={true} id="map">
+    <SDiv ref={mapRef}>
+      <MapContainer
+        key={center.toString()}
+        center={center}
+        zoom={10}
+        scrollWheelZoom={true}
+        id="map"
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        {markers.map((marker, index) => (
+          <Marker key={index} position={marker} />
+        ))}
+        <MapClickHandler />
       </MapContainer>
     </SDiv>
   );
