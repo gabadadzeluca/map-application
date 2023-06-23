daaad2a2d60a67aacb91b6e90cebf326fc8a23fd
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-06-23 20:34:15 +0400

    add fonts; format doc

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { GlobalStyle } from "./config/Global.styled.ts";
 import { Helmet } from "react-helmet";
-import './leaflet.css';
+import "./leaflet.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Helmet>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Pathway+Extreme:wght@400;500;600&display=swap"
+        rel="stylesheet"
+      />
       <script
         src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
         integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
